3ed620d3e7e52546762178368ac9fa64672b3db2
gxs <lyricsong47@gmail.com>
2025-05-21 16:11:28 +0800

    eat: add textContent to Meteor Admin SDK modal triggering

--- packages/admin-sdk/src/ui/modal/index.ts
+++ packages/admin-sdk/src/ui/modal/index.ts
@@ -4,27 +4,34 @@ export const open = createSender('uiModalOpen');
 export const close = createSender('uiModalClose');
 
 export type uiModalOpen =
- {
-   responseType: void,
-   title: string,
-   locationId: string,
-   variant?: 'default'|'small'|'large'|'full',
-   showHeader?: boolean,
-   showFooter?: boolean,
-   closable?: boolean,
-   buttons?: buttonProps[],
- }
+    {
+        responseType: void,
+        title: string,
+        // Use the locationId to render the modal in the iframe
+        locationId?: string,
+        // Use the textContent when no locationId is provided
+        textContent?: string,
+        variant?: 'default'|'small'|'large'|'full',
+        showHeader?: boolean,
+        showFooter?: boolean,
+        closable?: boolean,
+        buttons?: buttonProps[],
+    }
 
 export type uiModalClose =
- {
-   responseType: void,
-   locationId: string,
- }
+    {
+        responseType: void,
+        /*
+         * Define the locationId of the modal to close. If no locationId is provided,
+         * the last opened modal without a locationId will be closed.
+         */
+        locationId?: string,
+    }
 
 export type buttonProps = {
-  method: () => void,
-  label: string,
-  variant?: 'primary'|'ghost'|'danger'|'ghost-danger'|'contrast'|'context',
-  size?: 'x-small'|'small'|'large',
-  square?: boolean,
+    method: () => void,
+    label: string,
+    variant?: 'primary'|'ghost'|'danger'|'ghost-danger'|'contrast'|'context',
+    size?: 'x-small'|'small'|'large',
+    square?: boolean,
 }
